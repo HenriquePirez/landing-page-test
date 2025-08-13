1f4521653257d236f6f2537a2ed6dab3fda611f1
HenriquePirez <hpnogueira1@gmail.com>
2025-08-13 11:24:27 -0300

    feat: Add new background images and improve layout styles

--- script/scripts.js
+++ script/scripts.js
@@ -1,3 +1,18 @@
+// Pré-carrega imagens do carousel para melhorar performance
+  const preloadImages = [
+    'images/massas/pene com molho branco brocolis e frango.jpg',
+    'images/fundo-definitivo-camera.png',
+    'images/massas/penne quatro queijos.jpg',
+    'images/GAMBERI.jpg',
+    'images/DOLLCI CHOCOLATE.jpg',
+    'images/MARGUERITA.jpg',
+    'images/PALMITO.jpg'
+  ];
+  preloadImages.forEach(img => {
+    const image = new Image();
+    image.src = img;
+  });
+
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
